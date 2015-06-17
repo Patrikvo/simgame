6011ddebd90045ad422d5ed3d8fa46e863e2d284
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2015-06-17 13:30:06 +0200

    created view with correct camera

---
+++ Simgame2/Simgame2/WorldMap.cs
@@ -0,0 +1,227 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Simgame2
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class WorldMap : Microsoft.Xna.Framework.GameComponent
+    {
+        public WorldMap(Game game, int mapWidth, int mapHeight)
+            : base(game)
+        {
+            this.width = mapWidth;
+            this.height = mapHeight;
+
+            this.heightMap = new int[width * height];
+            generateMap();
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+
+
+
+        public int getCellAdress(int x, int y)
+        {
+            return (y * this.width) + x;
+        }
+
+        public int getCell(int x, int y)
+        {
+            return this.heightMap[getCellAdress(x, y)];
+        }
+
+        public void setCell(int x, int y, int value)
+        {
+            this.heightMap[getCellAdress(x, y)] = value;
+        }
+
+
+        public void lowerCellHeight(int x, int y, ushort amount)
+        {
+            // TODO implement
+        }
+
+        public void raiseCellHeight(int x, int y, ushort amount)
+        {
+            // TODO implement
+        }
+
+        public void generateMap()
+        {
+            Random rand = new Random();
+            // TODO implement better map generation
+
+            
+
+            // create water border
+            for (int x = 0; x < this.width; x++)
+            {
+                setCell(x, 0, 0);
+                setCell(x, height - 1, 0);
+
+            }
+            for (int y = 0; y < this.height; y++)
+            {
+                setCell(0, y, 0);
+                setCell(this.width - 1, y, 0);
+            }
+
+            // create beach border
+            for (int x = 1; x < this.width-1; x++)
+            {
+                setCell(x, 1, 1);
+                setCell(x, height - 2, 1);
+
+            }
+            for (int y = 1; y < this.height-1; y++)
+            {
+                setCell(1, y, 1);
+                setCell(this.width - 2, y, 1);
+            }
+
+            int modifier;
+            // create land mass
+            for (int y = 2; y < this.height-2; y++)
+            {
+                modifier = 1;
+                for (int x = 2; x < this.width-2; x++)
+                {
+                    //int randval = rand.Next(0, 100);
+
+                    modifier += rand.Next(-2, 2);
+                    if (modifier < 1) modifier = 1;
+                    if (modifier > 30) modifier = 30;
+
+                    setCell(x, y, modifier);
+                }
+            }
+
+
+            SetUpVertices();
+            SetUpIndices();
+
+            vertices = CalculateNormals(vertices, indices);
+
+        }
+
+
+
+        
+
+        private void SetUpVertices()
+        {
+            vertices = new VertexPositionNormalColored[this.width * this.height];
+            for (int x = 0; x < this.width; x++)
+            {
+                for (int y = 0; y < this.height; y++)
+                {
+                    vertices[x + y * this.width].Position = new Vector3(x, getCell(x, y), -y);
+
+                    if (getCell(x,y) == 0)
+                        vertices[x + y * this.width].Color = Color.Blue;
+                    else if (getCell(x, y) < 10)
+                        vertices[x + y * this.width].Color = Color.Green;
+                    else if (getCell(x, y) < 25)
+                        vertices[x + y * this.width].Color = Color.Brown;
+                    else
+                        vertices[x + y * this.width].Color = Color.White;
+
+                }
+            }
+        }
+
+        
+        private void SetUpIndices()
+        {
+            indices = new Int16[(this.width - 1) * (this.height - 1) * 6];
+            int counter = 0;
+            for (ushort y = 0; y < this.height - 1; y++)
+            {
+                for (ushort x = 0; x < this.width - 1; x++)
+                {
+                    Int16 lowerLeft = (Int16)(x + y * this.width);
+                    Int16 lowerRight = (Int16)((x + 1) + y * this.width);
+                    Int16 topLeft = (Int16)(x + (y + 1) * this.width);
+                    Int16 topRight = (Int16)((x + 1) + (y + 1) * this.width);
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = lowerRight;
+                    indices[counter++] = lowerLeft;
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = topRight;
+                    indices[counter++] = lowerRight;
+                }
+            }
+        }
+
+
+        private VertexPositionNormalColored[] CalculateNormals(VertexPositionNormalColored[] vertices, Int16[] indices)
+        {
+            for (int i = 0; i < vertices.Length; i++)
+                vertices[i].Normal = new Vector3(0, 0, 0);
+
+            for (int i = 0; i < indices.Length / 3; i++)
+            {
+                int index1 = indices[i * 3];
+                int index2 = indices[i * 3 + 1];
+                int index3 = indices[i * 3 + 2];
+
+                Vector3 side1 = vertices[index1].Position - vertices[index3].Position;
+                Vector3 side2 = vertices[index1].Position - vertices[index2].Position;
+                Vector3 normal = Vector3.Cross(side1, side2);
+
+                vertices[index1].Normal += normal;
+                vertices[index2].Normal += normal;
+                vertices[index3].Normal += normal;
+            }
+
+            for (int i = 0; i < vertices.Length; i++)
+                vertices[i].Normal.Normalize();
+
+            return vertices;
+        }
+
+
+        public int width { get; set; }
+        public int height { get; set; }
+
+        private int[] heightMap;
+
+        public VertexPositionNormalColored[] vertices;
+        public Int16[] indices;
+
+        public const int WaterLevel = 0;
+    }
+}
